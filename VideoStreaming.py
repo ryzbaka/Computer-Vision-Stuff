000ae393072100a8910f30a4d767537ae57c506a
Hamza Ali <hamzaali151214@hotmail.com>
2019-03-28 09:33:36 +0300

    Added video saving functionality

--- VideoStreaming.py
+++ VideoStreaming.py
@@ -1,12 +1,15 @@
 import cv2
 import numpy as np
+fourcc=cv2.VideoWriter_fourcc(*'XVID')
+out=cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
 
 cap=cv2.VideoCapture(0)
-
 while True:
     ret,frame=cap.read()
-    cv2.imshow('frame',frame)
+    out.write(frame)
+    cv2.imshow("frame",frame)
     if cv2.waitKey(1) & 0xFF==ord('q'):
         break
 cap.release()
-cv2.destroyAllWindows()
+out.release()
+cv2.destroyAllWindows() 
